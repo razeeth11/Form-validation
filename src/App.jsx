63e2cfd1788d47e17f45643df70746f886603640
razeeth11 <razeeth011@gmail.com>
2023-03-04 17:03:28 +0530

    formik&Yup

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import { Formik, useFormik } from 'formik';
+import * as yup from "yup";
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const formValidationSchema = yup.object({
+  username: yup.string().required(),
+  email: yup.string().required(),
+  password : yup.string().required(),
+});
 
+export function Form() {
+    
+
+    const formik = useFormik({
+    initialValues: {
+      username: "",
+      email : "",
+      password : ""
+    },
+    validationSchema : formValidationSchema,
+    onSubmit : (values) => console.log(values)
+  });
+  
   return (
-    <div className="Form">
-      
-    </div>
+       <form onSubmit={formik.handleSubmit}>
+       <TextField 
+       name="username"
+       onChange={formik.handleChange}
+       value={formik.values.username}
+       label="Username" variant="filled" />
+       <TextField 
+       name="email"
+       onChange={formik.handleChange}
+       value={formik.values.email}
+       label="E-mail" variant="filled" />
+       <TextField 
+       name="password"
+       onChange={formik.handleChange}
+       value={formik.values.password}
+       label="Password" variant="filled" />
+       <Button type='submit' variant="contained">Submit</Button>
+       </form>
   )
 }
 
-export default App
