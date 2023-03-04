64c9618c6f1b6fed437be28d19797fc16bc45d25
razeeth11 <razeeth011@gmail.com>
2023-03-04 18:35:33 +0530

    Validation

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,9 @@ import * as yup from "yup";
 import './App.css'
 
 const formValidationSchema = yup.object({
-  username: yup.string().required(),
+  username: yup.string().min(8).required(),
   email: yup.string().required(),
-  password : yup.string().required(),
+  password : yup.string().min(8).required(),
 });
 
 export function Form() {
@@ -28,18 +28,24 @@ export function Form() {
        <TextField 
        name="username"
        onChange={formik.handleChange}
+       onBlur={formik.handleBlur}
        value={formik.values.username}
        label="Username" variant="filled" />
+       {formik.touched.username && formik.errors.username ? formik.errors.username : null} 
        <TextField 
        name="email"
        onChange={formik.handleChange}
+       onBlur={formik.handleBlur}
        value={formik.values.email}
        label="E-mail" variant="filled" />
+       {formik.touched.email && formik.errors.email ? formik.errors.email : null} 
        <TextField 
        name="password"
        onChange={formik.handleChange}
+       onBlur={formik.handleBlur}
        value={formik.values.password}
        label="Password" variant="filled" />
+       {formik.touched.password && formik.errors.password ? formik.errors.password : null} 
        <Button type='submit' variant="contained">Submit</Button>
        </form>
   )
